ee90d08f63f3fa7aec1d7d670a3bd5828a0e929f
Daniel Anestopoulos <dstops97@gmail.com>
2023-10-05 16:34:00 -0400

    Add functioning search

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import weather from "./modules/weather";
 
 const searchForm = document.getElementById("searchForm");
+const locationInput = document.getElementById("locationInput");
+const searchBtn = document.getElementById("searchBtn");
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
+});
+
+searchBtn.addEventListener("click", async () => {
+  if (locationInput.value === "") return;
+  const weatherData = await weather.getWeather(locationInput.value);
+  locationInput.value = "";
+  console.log(weatherData);
 });
